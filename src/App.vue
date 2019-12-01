65d8d67fb244b44b5357a0239ded6cf548869cd4
liaojincheng <57680590+imliaojincheng@users.noreply.github.com>
2019-12-01 23:15:01 +0800

    0.1

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div><circle-loading /></div>
+  </div>
+</template>
+
+<script>
+import CircleLoading from './components/CircleLoading.vue'
+export default {
+  name: 'app',
+  components: {
+    CircleLoading
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
